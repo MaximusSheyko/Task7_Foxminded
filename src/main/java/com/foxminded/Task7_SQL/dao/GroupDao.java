f511079fb8e2e2b62a9265b6a1a0a5811417506c
Maxim Sheyko <maxim37377373@gmail.com>
2021-09-13 22:01:02 +0400

    look previous сommit

--- src/main/java/com/foxminded/Task7_SQL/dao/GroupDao.java
+++ src/main/java/com/foxminded/Task7_SQL/dao/GroupDao.java
@@ -11,10 +11,11 @@ public class GroupDao extends AbstractDao<Group> {
     
     private static final String SAVE_TO_TABLE = "INSERT INTO groups VALUES(default, ?)"; 
     private static final String GET_ALL_GROUPS = "SELECT * FROM groups";
+    private static final String COUNT_STUDENT = "SELECT count(students.group_id) AS amount "
+    	+ "FROM students WHERE students.group_id = ?";
     
     @Override
     public Group getById(int id) throws SQLException {
-	
 	return null;
     }
 
@@ -24,12 +25,12 @@ public class GroupDao extends AbstractDao<Group> {
     }
 
     @Override
-    public List<Group> getAllData() throws SQLException {
+    public List<Group> getAllData() {
 	List<Group> groups = new ArrayList<>();
 	
 	try(var con = DBCPDataSource.getConnection();
-	    var statement = con.prepareStatement(GET_ALL_GROUPS)){
-	    var resultSet = statement.executeQuery();
+	    var statement = con.prepareStatement(GET_ALL_GROUPS);
+	    var resultSet = statement.executeQuery()){
 	    
 	    while (resultSet.next()) {
 		groups.add
@@ -40,16 +41,44 @@ public class GroupDao extends AbstractDao<Group> {
 	    }
 	    
 	    return groups;
+	}catch (SQLException e) {
+	    e.getStackTrace();
 	}
+	
+	return groups;
     }
 
     @Override
-    public void save(Group group) throws SQLException {
+    public void save(Group group) {
 	try(var con = DBCPDataSource.getConnection();
 	    var statement = con.prepareStatement(SAVE_TO_TABLE)){
 	    statement.setString(1, group.getName());
 	    statement.executeUpdate();
 	}
+	catch (Exception e) {
+	    e.getStackTrace();
+	}
+    }
+    
+    public int countStudentInGroupById(int groupId) {
+	var id = groupId;
+	
+	try(var con = DBCPDataSource.getConnection();
+	    var statement = con.prepareStatement(COUNT_STUDENT)){
+	    statement.setInt(1, groupId);
+	    
+	    try(var resultSer = statement.executeQuery()){
+		if (resultSer.next()) {
+		    return resultSer.getInt("amount");
+		}else {
+		    id = 0;
+		}
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	
+	return groupId;
     }
 
     @Override
--- src/main/java/com/foxminded/Task7_SQL/service/GroupGenerator.java
+++ src/main/java/com/foxminded/Task7_SQL/service/GroupGenerator.java
@@ -1,13 +1,33 @@
 package com.foxminded.Task7_SQL.service;
 
+import java.util.Random;
+
+import com.foxminded.Task7_SQL.dao.GroupDao;
 import com.foxminded.Task7_SQL.entity.Group;
 import com.foxminded.Task7_SQL.service.interfaces.Generator;
 
-public class GroupGenerator implements Generator<Group>{
+public class GroupGenerator implements Generator<Group> {
+
+    private GroupDao groupDao;
+
+    public GroupGenerator(GroupDao groupDao) {
+	this.groupDao = groupDao;
+    }
 
     @Override
-    public void generate(Group group) {
-	
+    public void generate() {
+	var random = new Random();
+
+	for (var count = 0; count < 10; count++) {
+	    var groupIndex = Math.round(10 + Math.random() * 89);
+	    var groupName = getRandomChar() + getRandomChar() + "-" + groupIndex;
+	    groupDao.save(new Group.GroupBuilder().setName(groupName).build());
+	}
     }
     
+    private String getRandomChar() {
+  	var random = new Random();
+  	
+  	return String.valueOf((char)(random.nextInt(26) + 'a')).toUpperCase();
+    }
 }
